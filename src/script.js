a6507afa38810d3af1baaf388b98cb0b8750a352
marcnet80 <marcgit80@gmail.com>
2018-12-29 21:51:16 +0200

    formatting

--- src/script.js
+++ src/script.js
@@ -19,167 +19,146 @@ element.addEventListener("click", handleClick);
 
 init();
 
-function init()
-{
-	for(var row = 0; row < ROWS; row++){
+function init() {
+	for (var row = 0; row < ROWS; row++) {
 		cells[row] = [];
-    	for(var column = 0; column < COLUMNS; column++){
+		for (var column = 0; column < COLUMNS; column++) {
 			var x = CANVAS_RECT_WIDTH * row;
-            var y = CANVAS_RECT_HEIGHT * column;
-			
+			var y = CANVAS_RECT_HEIGHT * column;
+
 			cells[row][column] = { x: x, y: y, active: false };
 		}
-    }
+	}
 	displayGrid();
-    generationCount = 0;
+	generationCount = 0;
 	document.getElementById("#generation").innerHTML = generationCount;
-}  
-
-function handleClick(event)
-{
-   var x = Math.floor(event.offsetX/CANVAS_RECT_WIDTH)*CANVAS_RECT_WIDTH;
-   var y = Math.floor(event.offsetY/CANVAS_RECT_HEIGHT)*CANVAS_RECT_HEIGHT; 
-   cells[x/CANVAS_RECT_WIDTH][y/CANVAS_RECT_HEIGHT].active = cells[x/CANVAS_RECT_WIDTH][y/CANVAS_RECT_HEIGHT].active ? false: true;
-   displayGrid();
 }
 
- function hasCellActiveNeighbour(x, y) 
- {
-	if(!cells[x] || !cells[x][y])
-		  return false;
-	  
-    return cells[x][y].active;
+function handleClick(event) {
+	var x = Math.floor(event.offsetX / CANVAS_RECT_WIDTH) * CANVAS_RECT_WIDTH;
+	var y = Math.floor(event.offsetY / CANVAS_RECT_HEIGHT) * CANVAS_RECT_HEIGHT;
+	cells[x / CANVAS_RECT_WIDTH][y / CANVAS_RECT_HEIGHT].active = cells[x / CANVAS_RECT_WIDTH][y / CANVAS_RECT_HEIGHT].active ? false : true;
+	displayGrid();
 }
- 
- function countCellsNeighbours(x, y)
- {
-	    var neighbours = 0;
-		if (hasCellActiveNeighbour(x - POINT, y))
-		{ 
-		    neighbours++;
-		}
-        if (hasCellActiveNeighbour(x + POINT, y)) 
-		{	
-		    neighbours++;
-		}
-	    if (hasCellActiveNeighbour(x - POINT, y - POINT)) 
-		{
-			neighbours++;
-	    }
-		if (hasCellActiveNeighbour(x + POINT, y - POINT))
-	    {
-		    neighbours++;
-		}
-        if (hasCellActiveNeighbour(x, y - POINT))
-		{
-			neighbours++;
-		}
-        if (hasCellActiveNeighbour(x - POINT, y + POINT))
-		{
-		    neighbours++;
-		}
-        if (hasCellActiveNeighbour(x + POINT, y + POINT))
-		{
-			neighbours++;
-		}
-		if (hasCellActiveNeighbour(x, y + POINT))
-		{
-		    neighbours++;
-		}
-		
+
+function hasCellActiveNeighbour(x, y) {
+	if (!cells[x] || !cells[x][y])
+		return false;
+
+	return cells[x][y].active;
+}
+
+function countCellsNeighbours(x, y) {
+	var neighbours = 0;
+	if (hasCellActiveNeighbour(x - POINT, y)) {
+		neighbours++;
+	}
+	if (hasCellActiveNeighbour(x + POINT, y)) {
+		neighbours++;
+	}
+	if (hasCellActiveNeighbour(x - POINT, y - POINT)) {
+		neighbours++;
+	}
+	if (hasCellActiveNeighbour(x + POINT, y - POINT)) {
+		neighbours++;
+	}
+	if (hasCellActiveNeighbour(x, y - POINT)) {
+		neighbours++;
+	}
+	if (hasCellActiveNeighbour(x - POINT, y + POINT)) {
+		neighbours++;
+	}
+	if (hasCellActiveNeighbour(x + POINT, y + POINT)) {
+		neighbours++;
+	}
+	if (hasCellActiveNeighbour(x, y + POINT)) {
+		neighbours++;
+	}
+
 	return neighbours;
 }
-	
-function draw(cell)
-{
-   context.beginPath();
-   context.rect(cell.x, cell.y, CANVAS_RECT_WIDTH, CANVAS_RECT_HEIGHT);
-   
-  if(cell.active)
-  {
-   context.fillStyle = CANVAS_FILLSTYLE_ACTIVE_CELLS;
-   context.fill();
-  }
-  else 
-  {
-   context.fillStyle = CANVAS_FILLSTYLE_INACTIVE_CELLS;
-   context.strokeStyle = CANVAS_STROKESTYLE;
-   context.fill();
-   context.stroke();
-  }
-  
-  context.closePath();
+
+function draw(cell) {
+	context.beginPath();
+	context.rect(cell.x, cell.y, CANVAS_RECT_WIDTH, CANVAS_RECT_HEIGHT);
+
+	if (cell.active) {
+		context.fillStyle = CANVAS_FILLSTYLE_ACTIVE_CELLS;
+		context.fill();
+	}
+	else {
+		context.fillStyle = CANVAS_FILLSTYLE_INACTIVE_CELLS;
+		context.strokeStyle = CANVAS_STROKESTYLE;
+		context.fill();
+		context.stroke();
+	}
+
+	context.closePath();
 }
 
-function displayGrid()
-{
-   cells.forEach(function(element, row, array){
-       element.forEach(function(element2d, column, array2d){
-            draw(element2d);		  
-      });
-  });
+function displayGrid() {
+	cells.forEach(function (element, row, array) {
+		element.forEach(function (element2d, column, array2d) {
+			draw(element2d);
+		});
+	});
 }
-	
-function updateCells()
-{
+
+function updateCells() {
 	generationCount++;
 	newGenerationCells = [];
 	//clear existing rectangle
 	context.clearRect(0, 0, element.width, element.height);
 	displayGrid();
-	
-	if(!isCellsActive()){
-	   stopGame();
-	   return;
-	}
-   
-    cells.forEach(function(element, row, array){
-	  newGenerationCells[row] = [];
-	  
-	  element.forEach(function(element2d, column, array2d){
- 		  
-	  var isActive = false;	  
-	  var neighbours = countCellsNeighbours(row, column);
 
-	  if (element2d.active)
-        isActive = neighbours == (COUNT_NEIGHBOURS_FOR_ACTIVE_CELL - POINT) || neighbours == COUNT_NEIGHBOURS_FOR_ACTIVE_CELL ? true : false;
-      else
-		isActive = neighbours == COUNT_NEIGHBOURS_FOR_ACTIVE_CELL ? true : false;
-      
-	  var x = cells[row][column].x;
-	  var y = cells[row][column].y;
-	  newGenerationCells[row][column] = {x: x, y: y, active: isActive };	  
+	if (!isCellsActive()) {
+		stopGame();
+		return;
+	}
+
+	cells.forEach(function (element, row, array) {
+		newGenerationCells[row] = [];
+
+		element.forEach(function (element2d, column, array2d) {
+
+			var isActive = false;
+			var neighbours = countCellsNeighbours(row, column);
+
+			if (element2d.active)
+				isActive = neighbours == (COUNT_NEIGHBOURS_FOR_ACTIVE_CELL - POINT) || neighbours == COUNT_NEIGHBOURS_FOR_ACTIVE_CELL ? true : false;
+			else
+				isActive = neighbours == COUNT_NEIGHBOURS_FOR_ACTIVE_CELL ? true : false;
+
+			var x = cells[row][column].x;
+			var y = cells[row][column].y;
+			newGenerationCells[row][column] = { x: x, y: y, active: isActive };
+		});
+		document.getElementById("#generation").innerHTML = generationCount;
 	});
-	document.getElementById("#generation").innerHTML = generationCount;
-  });
-  
-  //copy new results
-  cells = newGenerationCells;
+
+	//copy new results
+	cells = newGenerationCells;
 }
 
-var isCellsActive = function()
-{
-  var isActiveCellsExist = cells.some(
-                              x=> x.some(cell =>
-                                 cell.active == true)); 
-  return isActiveCellsExist;
+var isCellsActive = function () {
+	var isActiveCellsExist = cells.some(
+		x => x.some(cell =>
+			cell.active == true));
+	return isActiveCellsExist;
 };
 
-function startGame()
-{
-     timer = setInterval(function(){ 
-        updateCells();
+function startGame() {
+	timer = setInterval(function () {
+		updateCells();
 	}, 20);
 }
 
-function stopGame()
-{
-	if(timer)
-	  clearInterval(timer);
+function stopGame() {
+	if (timer)
+		clearInterval(timer);
 }
 
-function clearGrid()
-{
+function clearGrid() {
 	stopGame();
 	init();
 }
